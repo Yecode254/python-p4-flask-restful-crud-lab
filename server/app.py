1830824cbbcd6b5938b29d19325961ce8e9e0881
Yego <eddieyego273@gmail.com>
2025-07-03 11:43:22 +0300

    complete

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, request, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
@@ -11,43 +12,64 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
-migrate = Migrate(app, db)
 db.init_app(app)
-
+migrate = Migrate(app, db)
 api = Api(app)
 
+# ROUTES
 
 class Plants(Resource):
-
     def get(self):
         plants = [plant.to_dict() for plant in Plant.query.all()]
         return make_response(jsonify(plants), 200)
 
     def post(self):
         data = request.get_json()
-
-        new_plant = Plant(
-            name=data['name'],
-            image=data['image'],
-            price=data['price'],
-        )
-
-        db.session.add(new_plant)
-        db.session.commit()
-
-        return make_response(new_plant.to_dict(), 201)
-
-
-api.add_resource(Plants, '/plants')
+        try:
+            new_plant = Plant(
+                name=data['name'],
+                image=data['image'],
+                price=data['price'],
+                is_in_stock=data.get('is_in_stock', True)
+            )
+            db.session.add(new_plant)
+            db.session.commit()
+            return make_response(new_plant.to_dict(), 201)
+        except Exception as e:
+            return make_response({'error': str(e)}, 400)
 
 
 class PlantByID(Resource):
-
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response({'error': 'Plant not found'}, 404)
+        return make_response(plant.to_dict(), 200)
 
 
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response({'error': 'Plant not found'}, 404)
+
+        data = request.get_json()
+        for attr in data:
+            if hasattr(plant, attr):
+                setattr(plant, attr, data[attr])
+        db.session.commit()
+        return make_response(plant.to_dict(), 200)
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response({'error': 'Plant not found'}, 404)
+
+        db.session.delete(plant)
+        db.session.commit()
+        return make_response('', 204)
+
+
+api.add_resource(Plants, '/plants')
 api.add_resource(PlantByID, '/plants/<int:id>')
 
 
--- server/testing/app_test.py
+++ server/testing/app_test.py
@@ -5,7 +5,20 @@ from models import db, Plant
 
 class TestPlant:
     '''Flask application in app.py'''
+    def setup_method(self):
+        with app.app_context():
+            db.drop_all()
+            db.create_all()
 
+            plant = Plant(
+                id=1,
+                name="Aloe",
+                image="./images/aloe.jpg",
+                price=11.50,
+                is_in_stock=True
+            )
+            db.session.add(plant)
+            db.session.commit()
     def test_plant_by_id_get_route(self):
         '''has a resource available at "/plants/<int:id>".'''
         response = app.test_client().get('/plants/1')
